09f87b1800ea1e216909753e73a9118e1e632a52
Anton Minin <aminin@progredis.ru>
2019-06-06 17:43:40 +0300

    Домашка

--- app.js
+++ app.js
@@ -1,36 +1,44 @@
-const express = require("express");
+const express = require('express');
 const fs = require('fs');
 
 const app = express();
 
-const dataFileName = "data/data.json";
-const boardsFileName = "data/boards.json";
+const dataFileName = 'data/data.json';
+const boardsFileName = 'data/boards.json';
+
+// Если файла нет — создаём
+if (!fs.existsSync(boardsFileName)) {
+    fs.writeFileSync(boardsFileName, '[]');
+}
 
 //let data = JSON.parse(fs.readFileSync(dataFileName).toString());
 
-//console.log("Data from file ", data);
+//console.log('Data from file ', data);
 app.listen(3000, () => {
- console.log("Server running on port 3000");
+ console.log('Server running on port 3000');
 });
 
-app.get("/boards", (req, res, next) => {
+// Главную страницу по адресу http://localhost:3000/
+app.use(express.static('.'));
+
+app.get('/boards', (req, res, next) => {
     let boardsData = JSON.parse(fs.readFileSync(boardsFileName).toString());
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.json(boardsData);
 });
 
 // Создать доску
-app.post("/boards", (req, res, next) => {
+app.post('/boards', (req, res, next) => {
     res.json('Реализуй меня');
 });
 
 // Создать доску
-app.post("/boards", (req, res, next) => {
+app.post('/boards', (req, res, next) => {
     res.json('Реализуй меня');
 });
 
 // Инфо о доске
-app.post("/boards/:id", (req, res, next) => {
+app.post('/boards/:id', (req, res, next) => {
     let id = req.params.id;
     res.json('Реализуй меня');
 });
